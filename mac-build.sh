cfaeeea00dca21ba084456b616cc80d908539fa9
nturume <nturumezz@gmail.com>
2025-09-29 21:28:43 +0300

    mac deploy

--- mac-build.sh
+++ mac-build.sh
@@ -4,5 +4,6 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 cmake --build .
 rm ZipCombiner.app/Contents/Info.plist
 cp ../mac/Info.plist ZipCombiner.app/Contents/
+macdeployqt ZipCombiner.app verbose=3
 cp -R ZipCombiner.app ~/Desktop
 
